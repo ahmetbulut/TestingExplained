faee8dbc6b82217a1ba6f082afab4a322f58be4c
bulut <ahmetbulut@sehir.edu.tr>
2019-11-07 12:48:02 +0300

    Revert "revert demo"
    This reverts commit 6527250fae39ba5a80ed3f88f2354d54823766bf.

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -5,4 +5,4 @@ from source import mymodule
 
 class MyTest(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(mymodule.hello_world(), "hello beautiful world")
+        self.assertEqual(mymodule.hello_world(), "hello my beautiful world")
