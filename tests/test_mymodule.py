7b77d332f2c8ca7e00bfbb5c15ffc7bcd9d8b12d
bulut <ahmetbulut@sehir.edu.tr>
2019-11-14 12:41:08 +0300

    Come on!

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -4,8 +4,5 @@ import unittest
 from source import mymodule
 
 class MyTest(unittest.TestCase):
-    def test_xyz(self):
-	self.assertEqual(2, 2)
-  
     def test_hello_world(self):
         self.assertEqual(mymodule.hello_world(), "hello beautiful world")
