5c805c73c1cf2c0822fbda92c22aba19742e6917
bulut <ahmetbulut@sehir.edu.tr>
2019-11-14 12:56:29 +0300

    Come on!

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -5,7 +5,7 @@ from source import mymodule
 
 class MyTest(unittest.TestCase):
     def test_xyz(self):
-        assert 2 == 2
+        self.assertGreater(3, 2)
 
     def test_hello_world(self):
         self.assertEqual(mymodule.hello_world(), "hello beautiful world")
