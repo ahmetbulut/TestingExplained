effcc28701c0c9dfede0d26cf0cf4427c26d77d3
bulut <ahmetbulut@sehir.edu.tr>
2019-11-14 13:22:06 +0300

    clean demo

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -5,4 +5,5 @@ from source import mymodule
 
 class MyTest(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(mymodule.hello_world(), "hello my beautiful world")
+	pass
+        #self.assertEqual(mymodule.hello_world(), "hello my beautiful world")
