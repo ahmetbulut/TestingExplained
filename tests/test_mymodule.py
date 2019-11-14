8eb03d5f83124c4910f46b7521331d5a5767f46c
bulut <ahmetbulut@sehir.edu.tr>
2019-11-14 12:40:31 +0300

    trying to replicate the conflict

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -5,7 +5,7 @@ from source import mymodule
 
 class MyTest(unittest.TestCase):
     def test_xyz(self):
-	self.assertEqual(2, 2)
+	self.assertEqual(4, 4)
   
     def test_hello_world(self):
         self.assertEqual(mymodule.hello_world(), "hello beautiful world")
