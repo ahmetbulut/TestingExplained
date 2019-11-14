168115e7f5020bf2b4377298ed39f1cb12e7a362
bulut <ahmetbulut@sehir.edu.tr>
2019-11-14 12:27:03 +0300

    My vs. Your world change.

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -4,8 +4,5 @@ import unittest
 from source import mymodule
 
 class MyTest(unittest.TestCase):
-    def test_xyz(self):
-        assert 2 == 2
-
     def test_hello_world(self):
-        self.assertEqual(mymodule.hello_world(), "hello my beautiful world")
+        self.assertEqual(mymodule.hello_world(), "hello your beautiful world")
