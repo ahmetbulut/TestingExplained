4aa02c79ccdb60b3a894318ff205f7631fa13126
bulut <ahmetbulut@sehir.edu.tr>
2019-11-14 12:46:35 +0300

    Come on!

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -3,6 +3,7 @@ __author__ = 'ahmetbulut'
 import unittest
 from source import mymodule
 
+
 class MyTest(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(mymodule.hello_world(), "hello beautiful world")
