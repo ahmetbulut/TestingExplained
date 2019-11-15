d3435315c43d9af2466f3664416cf3e398954a10
bulut <ahmetbulut@sehir.edu.tr>
2019-11-15 11:30:31 +0300

    Final version to upload onto LMS.

--- source/mymodule.py
+++ source/mymodule.py
@@ -1,4 +1,8 @@
 __author__ = 'ahmetbulut'
+__date__ = "Nov 2019"
+
+def take_square(param):
+    return param ** 2
 
 def hello_world():
     return "hello beautiful world"
--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -1,8 +1,16 @@
 __author__ = 'ahmetbulut'
+__date__ = "Nov 2019"
 
 import unittest
 from source import mymodule
 
 class MyTest(unittest.TestCase):
+    def test_take_square_greater(self):
+        self.assertGreater(mymodule.take_square(2), 2)
+
+    def test_take_square_equality(self):
+        self.assertEqual(mymodule.take_square(1), 1)
+        self.assertEqual(mymodule.take_square(0), 0)
+
     def test_hello_world(self):
-        self.assertGreater(22, 2)
+        self.assertEqual(mymodule.hello_world(), "hello beautiful world")
